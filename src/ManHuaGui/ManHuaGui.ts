5f0fea1b55bff07cd5f8f2cb7bacfd1e669c40ca
getBoolean <danielwnewville@gmail.com>
2021-08-16 01:04:53 -0500

    Create ManHuaGui, add manga details (wip)

---
+++ src/ManHuaGui/ManHuaGui.ts
@@ -0,0 +1,260 @@
+import {
+    Source,
+    Manga,
+    Chapter,
+    ChapterDetails,
+    HomeSection,
+    SearchRequest,
+    TagSection,
+    PagedResults,
+    SourceInfo,
+    MangaUpdates,
+    RequestHeaders,
+    TagType
+  } from "paperback-extensions-common"
+  import { generateSearch, isLastPage, parseChapterDetails, parseChapters, parseHomeSections, parseHotManga, parseNewManga, parseMangaDetails, parseSearch, parseTags, parseUpdatedManga, parseViewMore, UpdatedManga } from "./ManHuaGuiParser"
+  
+  const BG_DOMAIN = 'https://www.manhuagui.com';
+  // TODO: Support option to switch to traditional
+  const method = 'GET';
+  const headers = {
+      referer: BG_DOMAIN
+  };
+  
+  export const ManHuaGuiInfo: SourceInfo = {
+    version: '1.1.2',
+    name: 'ManHuaGui (漫画柜)',
+    icon: 'favicon.ico',
+    author: 'getBoolean',
+    authorWebsite: 'https://github.com/getBoolean',
+    description: 'Extension that pulls manga from ManHuaGui',
+    hentaiSource: false,
+    websiteBaseURL: BG_DOMAIN,
+    sourceTags: [
+        // {
+        //     text: "Notifications",
+        //     type: TagType.GREEN
+        // },
+        {
+            text: "中文",
+            type: TagType.GREY
+        }
+    ]
+  }
+  
+  export class BainianManga extends Source {
+    getMangaShareUrl(mangaId: string): string | null { return `${BG_DOMAIN}/comic/${mangaId}` }
+  
+    async getMangaDetails(mangaId: string): Promise<Manga> {
+        const request = createRequestObject({
+            url: `${BG_DOMAIN}/comic/`,
+            method,
+            param: `${mangaId}/`
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+
+        // TODO: Add option to use Chinese titles when available
+        let result : [Manga, string] = parseMangaDetails($, mangaId)
+
+        return result[0]
+    }
+  
+
+    async getChapters(mangaId: string): Promise<Chapter[]> {
+        const request = createRequestObject({
+            url: `${BM_DOMAIN}/comic/`,
+            method,
+            param: `${mangaId}.html`
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        return parseChapters($, mangaId)
+    }
+  
+
+    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+        let request = createRequestObject({
+            url: `${BM_DOMAIN}/comic/`,
+            method,
+            headers,
+            param: `${mangaId}/${chapterId}.html`
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+
+        return parseChapterDetails($, mangaId, chapterId, response.data)
+    }
+  
+
+    async filterUpdatedManga(mangaUpdatesFoundCallback: (updates: MangaUpdates) => void, time: Date, ids: string[]): Promise<void> {
+        let page = 1
+            let updatedManga: UpdatedManga = {
+            ids: [],
+            loadMore: true
+        }
+
+        while (updatedManga.loadMore) {
+        const request = createRequestObject({
+            url: `${BM_DOMAIN}/page/new/`,
+            method,
+            headers,
+            param: `${String(page++)}.html`
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        updatedManga = parseUpdatedManga($, time, ids)
+
+        if (updatedManga.ids.length > 0) {
+            mangaUpdatesFoundCallback(createMangaUpdates({
+            ids: updatedManga.ids
+            }))
+        }
+        }
+    }
+  
+ 
+    async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        // Give Paperback a skeleton of what these home sections should look like to pre-render them
+
+        const sectionRequests = [
+            {
+                request: createRequestObject({
+                    url: `${BM_DOMAIN}/comic.html`,
+                    method: 'GET'
+                }),
+                section: createHomeSection({
+                    id: 'a_recommended',
+                    title: '推荐漫画'
+                }),
+            },
+            {
+                request: createRequestObject({
+                    url: `${BM_DOMAIN}/page/hot/1.html`,
+                    method: 'GET'
+                }),
+                section: createHomeSection({
+                    id: 'hot_comics',
+                    title: '热门漫画',
+                    view_more: true,
+                }),
+            },
+            {
+                request: createRequestObject({
+                    url: `${BM_DOMAIN}/page/new/1.html`,
+                    method: 'GET'
+                }),
+                section: createHomeSection({
+                    id: 'z_new_updates',
+                    title: '最近更新',
+                    view_more: true
+                }),
+            },
+        ]
+
+        const promises: Promise<void>[] = []
+
+        for (const sectionRequest of sectionRequests) {
+            // Let the app load empty sections
+            sectionCallback(sectionRequest.section)
+
+            // Get the section data
+            promises.push(
+                this.requestManager.schedule(sectionRequest.request, 1).then(response => {
+                    const $ = this.cheerio.load(response.data)
+                    switch(sectionRequest.section.id) {
+                        case 'a_recommended':
+                            sectionRequest.section.items = parseHomeSections($)
+                            break
+                        case 'hot_comics':
+                            sectionRequest.section.items = parseHotManga($)
+                            break
+                        case 'z_new_updates':
+                            sectionRequest.section.items = parseNewManga($)
+                            break
+                        default:
+                    }
+                    sectionCallback(sectionRequest.section)
+                }),
+            )
+        }
+
+        // Make sure the function completes
+        await Promise.all(promises)
+    }
+  
+
+    async searchRequest(query: SearchRequest, metadata: any): Promise<PagedResults> {
+        let page : number = metadata?.page ?? 1
+        const search = generateSearch(query)
+        const request = createRequestObject({
+            url: `${BM_DOMAIN}/search/`,
+            method,
+            headers,
+            param: `${search}/${page}.html`
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        const manga = parseSearch($)
+        metadata = !isLastPage($) ? {page: page + 1} : undefined
+        
+        return createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+  
+
+    async getTags(): Promise<TagSection[] | null> {
+        const request = createRequestObject({
+            url: `${BM_DOMAIN}/page/list.html`,
+            method,
+            headers,
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        return parseTags($)
+    }
+  
+
+    async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults | null> {
+        // console.log('getViewMoreItems($)')
+        let page : number = metadata?.page ?? 1
+        let param = ''
+        if (homepageSectionId === 'hot_comics')
+            param = `/page/hot/${page}.html`
+        else if (homepageSectionId === 'z_new_updates')
+            param = `/page/new/${page}.html`
+        else return Promise.resolve(null)
+
+        const request = createRequestObject({
+            url: `${BM_DOMAIN}`,
+            method,
+            param,
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        const manga = parseViewMore($)
+        // console.log('isLastPage($) ' + isLastPage($))
+        metadata = !isLastPage($) ? { page: page + 1 } : undefined
+
+        return createPagedResults({
+            results: manga,
+            metadata
+        })
+    }
+  
+
+    globalRequestHeaders(): RequestHeaders {
+        return {
+            referer: BM_DOMAIN
+        }
+    }
+  }
---
+++ src/ManHuaGui/ManHuaGuiParser.ts
@@ -0,0 +1,283 @@
+import { Chapter, ChapterDetails, HomeSection, LanguageCode, Manga, MangaStatus, MangaTile, MangaUpdates, PagedResults, SearchRequest, TagSection } from "paperback-extensions-common";
+
+const ChineseNumber = require('./external/chinese-numbers.js');
+
+export const parseMangaDetails = ($: CheerioStatic, mangaId: string): [Manga, string] => {
+    const page = $('div.w998.bc.cf') ?? '';
+    const infoBox = $('div.book-cont', page);
+    const chineseTitle : string = $('div.book-title h2', infoBox).text();
+    const englishTitle : string = $('div.book-title h1', infoBox).text();
+    const isChineseTitleEmpty : boolean = chineseTitle.length == 0;
+    const title : string = isChineseTitleEmpty ? englishTitle : chineseTitle;
+    const altTitle : string = isChineseTitleEmpty ? '' : englishTitle;
+    const image : string = $('div.book-cover p.hcover img', infoBox).attr('src') ?? '';
+
+    const bookDetails = $('div.book-detail ul.detail-list', infoBox);
+    let author = $('li:nth-child(2) span:nth-child(2) a', bookDetails).attr('title') ?? ''
+    let artist = ''
+    let rating = $('p.score-avg em', page).text()
+
+    // 连载中: Currently serializing
+    // 已完结: Finished
+    let status = $('li.status span span', bookDetails).first().text() == '连载中' ? MangaStatus.ONGOING : MangaStatus.COMPLETED
+    let titles = isChineseTitleEmpty ? [title] : [title, altTitle];
+    let follows = 0
+    let views = 0
+    let lastUpdate = $('li.status span span:nth-child(3)', bookDetails).text()
+    let hentai = false
+
+    const tagSections: TagSection[] = [createTagSection({ id: '0', label: 'genres', tags: [] })]
+    
+    // TODO: Add genres
+    const elems = $('.yac', infoElement).find('a').toArray()
+    tagSections[0].tags = elems.map((elem) => createTag({ id: $(elem).text(), label: $(elem).text() }))
+
+    const time = new Date(lastUpdate)
+    lastUpdate = time.toDateString()
+
+    const summary = $('div.book-intro div[id="intro-all"] p', page).text()
+
+    return [createManga({
+        id: mangaId,
+        titles,
+        image,
+        rating: Number(rating),
+        status,
+        artist,
+        author,
+        tags: tagSections,
+        views,
+        follows,
+        lastUpdate,
+        desc: summary,
+        hentai
+    }), image]
+}
+
+
+export const parseChapters = ($: CheerioStatic, mangaId: string): Chapter[] => {
+    const json = $('[type=application\\/ld\\+json]').html()?.replace(/\t*\n*/g, '') ?? ''
+    const parsedJson = JSON.parse(json)
+    const time = new Date(parsedJson.upDate) // Set time for all chapters to be the last updated time
+
+    const allChapters = $('li', '.list_block ').toArray()
+    const chapters: Chapter[] = []
+    
+    for (let chapter of allChapters) {
+        const id: string = ( $('a', chapter).attr('href')?.split('/').pop() ?? '' ).replace('.html', '')
+        const name: string = $('a', chapter).text() ?? ''
+        const convertedString = new ChineseNumber(name).toArabicString();
+        const chapNum: number = Number(convertedString.match(/\d+/) ?? 0 )
+        chapters.push(createChapter({
+            id,
+            mangaId,
+            name,
+            langCode: LanguageCode.CHINEESE,
+            chapNum,
+            time
+        }))
+    }
+    return chapters
+}
+
+
+export const parseChapterDetails = ($: CheerioStatic, mangaId: string, chapterId: string, data: any): ChapterDetails => {
+    const json = $('[type=application\\/ld\\+json]').html()?.replace(/\t*\n*/g, '') ?? '';
+    const parsedJson = JSON.parse(json)
+    const firstImageUrl = parsedJson.images[0];
+    const baseUrlStartingPosition : number = firstImageUrl.indexOf('//') + 2;
+    const baseImageURL = firstImageUrl.slice(0, firstImageUrl.indexOf('/', baseUrlStartingPosition));
+    const imageCodes = data?.match(/var z_img='(.*?)';/)?.pop();
+
+    let pages : string[] = []
+    if (imageCodes) {
+        const imagePaths = JSON.parse(imageCodes) as string[]
+        pages = imagePaths.map(imagePath => `${baseImageURL}/${imagePath}`)
+    }
+    // console.log(pages)
+
+    return createChapterDetails({
+        id: chapterId,
+        mangaId: mangaId,
+        pages,
+        longStrip: false
+    })
+}
+
+
+export interface UpdatedManga {
+    ids: string[];
+    loadMore: boolean;
+}
+
+
+export const parseUpdatedManga = ($: CheerioStatic, time: Date, ids: string[]): UpdatedManga => {
+    const foundIds: string[] = []
+    let passedReferenceTime = false
+    const panel = $('.tbox_m')
+    const allItems = $('.vbox', panel).toArray()
+    for (const item of allItems) {
+        const id = (($('a', item).first().attr('href') ?? '').split('/').pop() ?? '' ).replace('.html', '')
+        let mangaTime = new Date($($(item).find('h4')[1]).text())
+
+        passedReferenceTime = mangaTime > time
+        if (passedReferenceTime) {
+            if (ids.includes(id)) {
+                foundIds.push(id)
+            }
+        }
+        else break
+    }
+
+    return {
+        ids: foundIds,
+        loadMore: passedReferenceTime
+    }
+}
+
+
+export const parseHomeSections = ($: CheerioStatic): MangaTile[] => {
+    const recommendedManga: MangaTile[] = []
+
+    // Recommended
+    const grid = $('.tbox_m')[0]
+    const allItems = $('.vbox', grid).toArray()
+    for (const item of allItems) {
+        const id = (($('a', item).first().attr('href') ?? '').split('/').pop() ?? '' ).replace('.html', '')
+        const title = $('.vbox_t', item).attr('title') ?? 'No title'
+        const subtitle = $('.vbox_t span', item).text()
+        const image = $('.vbox_t mip-img', item).attr('src') ?? ''
+
+        recommendedManga.push(createMangaTile({
+            id,
+            image,
+            title: createIconText({ text: title }),
+            subtitleText: createIconText({ text: subtitle })
+        }))
+    }
+
+    return recommendedManga
+}
+
+
+export const parseHotManga = ($: CheerioStatic): MangaTile[] => {
+    const hotManga: MangaTile[] = []
+
+    // New
+    const grid = $('.tbox_m')[0]
+    const allItems = $('.vbox', grid).toArray()
+    for (const item of allItems) {
+        const id = (($('a', item).first().attr('href') ?? '').split('/').pop() ?? '' ).replace('.html', '')
+        const title = $('.vbox_t', item).attr('title') ?? 'No title'
+        const subtitle = $('.vbox_t span', item).text()
+        const image = $('.vbox_t mip-img', item).attr('src') ?? ''
+
+        hotManga.push(createMangaTile({
+            id,
+            image,
+            title: createIconText({ text: title }),
+            subtitleText: createIconText({ text: subtitle })
+        }))
+    }
+
+    return hotManga
+}
+
+
+export const parseNewManga = ($: CheerioStatic): MangaTile[] => {
+    const newManga: MangaTile[] = []
+
+    // New
+    const grid = $('.tbox_m')[0]
+    const allItems = $('.vbox', grid).toArray()
+    for (const item of allItems) {
+        const id = (($('a', item).first().attr('href') ?? '').split('/').pop() ?? '' ).replace('.html', '')
+        const title = $('.vbox_t', item).attr('title') ?? 'No title'
+        const subtitle = $('.vbox_t span', item).text()
+        const image = $('.vbox_t mip-img', item).attr('src') ?? ''
+
+        newManga.push(createMangaTile({
+            id,
+            image,
+            title: createIconText({ text: title }),
+            subtitleText: createIconText({ text: subtitle })
+        }))
+    }
+
+    return newManga
+}
+
+
+export const generateSearch = (query: SearchRequest): string => {
+
+    let keyword = (query.title ?? '').replace(/ /g, '+')
+    if (query.author)
+        keyword += (query.author ?? '').replace(/ /g, '+')
+    let search: string = `${keyword}`
+
+    return search
+}
+
+
+export const parseSearch = ($: CheerioStatic): MangaTile[] => {
+    const panel = $('.tbox_m')
+    const allItems = $('.vbox', panel).toArray()
+    const manga: MangaTile[] = []
+    for (const item of allItems) {
+        const id = (($('a', item).first().attr('href') ?? '').split('/').pop() ?? '' ).replace('.html', '')
+        const title = $('.vbox_t', item).attr('title') ?? 'No title'
+        const subtitle = $('.vbox_t span', item).text()
+        const image = $('.vbox_t mip-img', item).attr('src') ?? ''
+
+        manga.push(createMangaTile({
+            id,
+            image,
+            title: createIconText({ text: title }),
+            subtitleText: createIconText({ text: subtitle }),
+        }))
+    }
+    return manga
+}
+
+
+export const parseTags = ($: CheerioStatic): TagSection[] | null => {
+    const allItems = $('.tbox a').toArray()
+    const genres = createTagSection({
+        id: 'genre',
+        label: 'Genre',
+        tags: []
+    })
+    for (let item of allItems) {
+        let label = $(item).text()
+        genres.tags.push(createTag({ id: label, label: label }))
+    }
+    return [genres]
+}
+
+
+export const parseViewMore = ($: CheerioStatic): MangaTile[] => {
+    console.log('parseViewMore($)')
+    const panel = $('.tbox_m')
+    const allItems = $('.vbox', panel).toArray()
+    const manga: MangaTile[] = []
+    for (const item of allItems) {
+        const id = (($('a', item).first().attr('href') ?? '').split('/').pop() ?? '' ).replace('.html', '')
+        const title = $('.vbox_t', item).attr('title') ?? 'No title'
+        const subtitle = $('.vbox_t span', item).text()
+        const image = $('.vbox_t mip-img', item).attr('src') ?? ''
+
+        manga.push(createMangaTile({
+            id,
+            image,
+            title: createIconText({ text: title }),
+            subtitleText: createIconText({ text: subtitle }),
+        }))
+    }
+    return manga
+}
+
+
+export const isLastPage = ($: CheerioStatic): boolean => {
+    // see if the button is disabled
+    return $('li', $('.pagination')).last().hasClass('disabled');
+}
